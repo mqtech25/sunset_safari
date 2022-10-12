3f1c18c311087d72f951795fd856ad884cf63d9a
Muhammad Qasim <mqtech25@gmail.com>
2022-10-12 20:46:37 +0500

    add detail view pages

---
+++ app/Http/Controllers/Site/ServiceController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Site;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ServiceController extends Controller
+{
+    //
+    public function show()
+    {
+        return view('site.pages.servicedetailpage');
+    }
+}
